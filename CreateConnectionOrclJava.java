8ecd75c8458c287547c7a8a6706f31a10219baaa
nadiamusawa <7846554+nadiamusawa@users.noreply.github.com>
2019-11-08 18:21:36 +0700

    Update CreateConnectionOrclJava.java

--- CreateConnectionOrclJava.java
+++ CreateConnectionOrclJava.java
@@ -1,1 +1,31 @@
+package oracle_oop;
 
+import java.sql.*;
+
+public class CreateConnectionOrclJava {
+	
+	public static  Connection getConnection() {
+		Connection conn = null;
+		
+		String url 	= "jdbc:oracle:thin:@localhost:1521:xe";
+		String user	= "oracle";
+		String pass	= "p@ssword";
+		
+		try {
+			conn = DriverManager.getConnection(url,user, pass);
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return conn;
+	}
+
+	public static void main(String[] args) {
+		if (getConnection() != null) {
+			System.out.println("Connected!");
+		}
+		
+
+	}
+
+}
